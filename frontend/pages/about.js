dd29c9f10a2ac9a7b949206e3f45ccc6589a3d21
Sam Marxz <sammarxzz@gmail.com>
2020-12-01 11:27:36 -0300

    Tests in Next Routing.

---
+++ frontend/pages/about.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'next/link'
+
+const About = () => (
+  <>
+    <h1>About</h1>
+    <Link href="/">
+      <a>Home</a>
+    </Link>
+  </>
+)
+
+export default About
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React from 'react'
+import Link from 'next/link'
 
 const Index = () => (
-  <h1>Home</h1>
+  <>
+    <h1>Home</h1>
+    <Link href="/about">
+      <a>About</a>
+    </Link>
+  </>
 )
 
-export default Index;
+export default Index
