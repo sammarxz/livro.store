6ab37871d4a5c8ceff71da52dc01dd261f427a1a
Sam Marxz <sammarxzz@gmail.com>
2020-12-02 12:19:30 -0300

    Add theme provider.

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,15 +1,32 @@
+import { ThemeProvider } from 'styled-components'
+
 import { Layout } from '../components'
 
-// Global Styles
 import GlobalStyle from '../styles/global'
+
+const theme = {
+  colors: {
+    primary: '#E2910E',
+    secondary: '#194188',
+    gray: '#7B7B7B',
+    midGray: '#9E9E9E',
+    lightGray: '#EFEFEF'
+  },
+  fonts: {
+    primary: 'Inter'
+  },
+  baseMargin: '8px'
+}
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <GlobalStyle />
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <ThemeProvider theme={theme}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
     </>
   )
 }
